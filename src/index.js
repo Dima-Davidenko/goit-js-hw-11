245598f9b4f7beed846ba58ee672c496b8c16dce
Dmytro Davidenko <dima080885@gmail.com>
2022-11-18 17:46:12 +0200

    working. without styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,57 @@
-import { pixabayKey } from './js/utils/envConsts.js';
-import { a } from './temp.js';
+import { fetchPixabayImages } from './js/fetchPixabay.js';
+import { IMAGES_PER_PAGE } from './js/utils/envConsts.js';
+import { Notify } from 'notiflix';
+import throttle from 'lodash.throttle';
+import cardTpl from './js/templates/cardTpl.hbs';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.css';
 
-console.log(a);
+const formEl = document.querySelector('.search-form');
+const galleryEl = document.querySelector('.gallery');
+let page = null;
+let query = null;
+let imagesAvailable = null;
+const throttledHandlerDocumentScroll = throttle(handleDocumentScroll, 500);
 
-console.log(pixabayKey);
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  animationSlide: false,
+  animationSpeed: 500,
+  maxZoom: 5,
+});
+
+function handleFormSubmit(e) {
+  e.preventDefault();
+  page = 1;
+  query = e.target.elements.searchQuery.value.trim();
+  fetchPixabayImages(query, page).then(({ hits, totalHits }) => {
+    galleryEl.innerHTML = '';
+    if (!hits.length) {
+      Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+      return;
+    }
+    imagesAvailable = totalHits;
+    const markup = hits.map(image => cardTpl(image)).join('');
+    galleryEl.innerHTML = markup;
+    lightbox.refresh();
+    Notify.info(`Hooray! We found ${totalHits} images.`);
+  });
+}
+
+function handleDocumentScroll({ target }) {
+  if (target.documentElement.scrollHeight - target.documentElement.scrollTop < 2000) {
+    page += 1;
+    fetchPixabayImages(query, page).then(({ hits }) => {
+      const markup = hits.map(image => cardTpl(image)).join('');
+      galleryEl.insertAdjacentHTML('beforeend', markup);
+      lightbox.refresh();
+    });
+  }
+  if (page * IMAGES_PER_PAGE >= imagesAvailable) {
+    document.removeEventListener('scroll', throttledHandlerDocumentScroll);
+  }
+}
+
+formEl.addEventListener('submit', handleFormSubmit);
+document.addEventListener('scroll', throttledHandlerDocumentScroll);
--- src/js/utils/envConsts.js
+++ src/js/utils/envConsts.js
@@ -2,3 +2,4 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export const pixabayKey = process.env.PIXABAY_SECRET_KEY;
+export const IMAGES_PER_PAGE = 10;
